35c9d5f5d8d8b8d7e25aa12cd57d482da6cc7495
Hamza <jhamzaahmed@gmail.com>
2023-12-09 18:49:18 +0500

    completed desktop design

--- faq-accordion-main/js/app.js
+++ faq-accordion-main/js/app.js
@@ -12,12 +12,14 @@ qWrapper.forEach((question) => {
     );
 
     if (flag) {
-      main.style.height = main.offsetHeight + 100 + "px";
-      answer.style.display = "block";
+      main.style.height = main.offsetHeight + 90 + "px";
       icon.src = "./assets/images/icon-minus.svg";
       flag = false;
+      setTimeout(() => {
+        answer.style.display = "block";
+      }, 200);
     } else {
-      main.style.height = main.offsetHeight - 100 + "px";
+      main.style.height = main.offsetHeight - 90 + "px";
       answer.style.display = "none";
       icon.src = "./assets/images/icon-plus.svg";
       flag = true;
