dd8ea6a3c9792c4bc775bad14a8f462146625e8a
Hamza <jhamzaahmed@gmail.com>
2024-03-24 15:44:45 +0500

    created mobile design

--- four-card-feature-section-master/src/App.jsx
+++ four-card-feature-section-master/src/App.jsx
@@ -1,26 +1,74 @@
+/* eslint-disable react/prop-types */
 import "./App.css";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      name: "Supervisor",
+      desc: "Monitors activity to identify project roadblocks",
+      img: "icon-supervisor.svg",
+      color: "cyan",
+    },
+    {
+      id: 2,
+      name: "Team Builder",
+      desc: "Scans our talent network to create the optimal team for your project",
+      img: "icon-team-builder.svg",
+      color: "red",
+    },
+    {
+      id: 3,
+      name: "Karma",
+      desc: "Regularly evaluates our talent to ensure quality",
+      img: "icon-karma.svg",
+      color: "orange",
+    },
+    {
+      id: 4,
+      name: "Calculator",
+      desc: "Uses data from past projects to provide better delivery estimates",
+      img: "icon-calculator.svg",
+      color: "blue",
+    },
+  ];
+
   return (
-    <>
-      <h1 className="text-center mt-[5rem] text-2xl font-light opacity-70">
+    <div className="px-10">
+      <h1 className="text-center mt-[5rem] text-2xl font-light opacity-70 ">
         Reliable, efficient delivery <br />{" "}
         <span className="font-bold">Powered by Technology</span>
       </h1>
-      <p className="text-center mt-5 px-14 text-gray-400">
+      <p className="text-center mx-auto mt-5 mb-16 px-4 text-gray-400 font-semibold sm:w-[50%]">
         Our Artificial Intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-    </>
+      <div className="sm:grid sm:grid-rows-2 sm:grid-cols-3 sm:gap-6 sm:grid-flow-col">
+        {data.map((obj) => (
+          <Box
+            key={obj.id}
+            name={obj.name}
+            desc={obj.desc}
+            imgPath={obj.img}
+            color={obj.color}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
-function Box() {
-  return <div>Box</div>;
+function Box({ name, desc, imgPath, color }) {
+  return (
+    <div
+      className="border-t-[3.5px] rounded shadow-custom border-[--color] my-8 p-6"
+      style={{ "--color": color }}
+    >
+      <h3 className="font-bold text-xl opacity-70">{name}</h3>
+      <p className="text-sm text-gray-400 my-1">{desc}</p>
+      <img src={"/images/" + imgPath} className="ml-auto mt-10" alt="" />
+    </div>
+  );
 }
 
 export default App;
--- four-card-feature-section-master/tailwind.config.js
+++ four-card-feature-section-master/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "hsl(0, 78%, 62%)",
+        cyan: "hsl(180, 62%, 55%)",
+        blue: "hsl(212, 86%, 64%)",
+        orange: "hsl(34, 97%, 64%)",
+        "dark-blue": "hsl(234, 12%, 34%)",
+        "grayish-blue": "hsl(229, 6%, 66%)",
+        "light-gray": "hsl(0, 0%, 98%)",
+      },
+      boxShadow: {
+        custom: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+      },
+    },
   },
   plugins: [],
 };
