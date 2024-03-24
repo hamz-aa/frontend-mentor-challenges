a01bb5d1c09df7f4c723767fe96b00f816bb90bf
Hamza <jhamzaahmed@gmail.com>
2024-03-24 22:18:24 +0500

    completed project

--- four-card-feature-section-master/src/App.jsx
+++ four-card-feature-section-master/src/App.jsx
@@ -35,15 +35,15 @@ function App() {
 
   return (
     <div className="px-10">
-      <h1 className="text-center mt-[5rem] text-2xl font-light opacity-70 ">
+      <h1 className="text-center mt-[5rem] text-2xl font-thin opacity-70 ">
         Reliable, efficient delivery <br />{" "}
         <span className="font-bold">Powered by Technology</span>
       </h1>
-      <p className="text-center mx-auto mt-5 mb-16 px-4 text-gray-400 font-semibold sm:w-[50%]">
+      <p className="text-center mx-auto mt-5 mb-16 sm:mb-8 px-4 text-grayishBlue font-normal sm:w-[50%]">
         Our Artificial Intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
-      <div className="sm:grid sm:grid-rows-2 sm:grid-cols-3 sm:gap-6 sm:grid-flow-col">
+      <div className="sm:grid sm:grid-rows-2 sm:grid-cols-3 sm:grid-flow-col sm:items-center sm:justify-center sm:[&>*:nth-child(3)]:mb-16 sm:[&>*:nth-child(1)]:row-span-2 sm:[&>*:nth-child(4)]:row-span-2 sm:[&>*:nth-child(2)]:row-span-1 sm:[&>*:nth-child(3)]:row-span-1">
         {data.map((obj) => (
           <Box
             key={obj.id}
@@ -61,7 +61,7 @@ function App() {
 function Box({ name, desc, imgPath, color }) {
   return (
     <div
-      className="border-t-[3.5px] rounded shadow-custom border-[--color] my-8 p-6"
+      className="border-t-[3.5px] rounded w-[90%] justify-self-center shadow-custom border-[--color] my-8 sm:my-0 p-6 "
       style={{ "--color": color }}
     >
       <h3 className="font-bold text-xl opacity-70">{name}</h3>
